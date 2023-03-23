6017033825e567dffd9411307960a8d727639f0a
dlopez1591 <dlopez1591@gmail.com>
2023-03-23 13:12:07 -0500

    intelli iniciado

---
+++ src/test/java/com/foodieparty/fodieParty/RestauranteFoodiePartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodieparty.fodieParty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteFoodiePartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
