be99ab20ed31edd11d0a62b9a19e67602fec74ef
sulbd <joaquinfleitas71@gmail.com>
2023-03-29 20:56:33 -0300

    Login y Register

--- src/main/resources/static/web/assets/scripts/index.js
+++ src/main/resources/static/web/assets/scripts/index.js
@@ -1,8 +1,73 @@
 const { createApp } = Vue
+
 createApp({
     data() {
-      return {
-       
-      }
+        return {
+            email: '',
+            password: '',
+            error: '',
+            firstName: '',
+            lastName: '',
+            cambio: 1,
+        }
+    },
+    methods: {
+        login() {
+            axios.post('/api/login', `email=${this.email}&password=${this.password}`, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+                .then(response => {
+                    Swal.fire({
+                        position: 'midle',
+                        icon: 'success',
+                        color: 'var(--bs-highlight-bg)',
+                        background: '#000000c1',
+                        title: 'Your login have been successful',
+                        showConfirmButton: false,
+                        timer: 2000
+                    })
+                        .then(() => {
+                            if (this.email == "admin@gmail.com") {
+                                window.location.href = '/manager.html';
+                            } else {
+                                window.location.href = '/web/accounts.html';
+                            }
+                        })
+                })
+                .catch(error => {
+                    this.error = error.response.data.message;
+                    Swal.fire({
+                        icon: 'error',
+                        color: 'var(--bs-highlight-bg)',
+                        background: '#000000c1',
+                        title: 'Oops... something wrongs',
+                        text: 'Show your dates!',
+                    })
+                });
+        },
+        cambiar() {
+            if (this.cambio == 0)
+                this.cambio = 1;
+            else
+                this.cambio = 0
+        },
+        register() {
+            if (/^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/.test(this.email)) {
+                axios.post('/api/clients', `first=${this.firstName}&lastName=${this.lastName}&email=${this.email}&password=${this.password}`, {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                })
+                    .then(response => {
+                        this.login();
+                    })
+                    .catch(error => {
+                        this.error = error.response.data.message;
+                    });
+            }
+
+        },
     }
-  }).mount('#app')
+}).mount('#app')
