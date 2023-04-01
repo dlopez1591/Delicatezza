f7ad0a7d93e18790ece767b37e67626efa571c62
YasminLedesma <yasminledesma9@gmail.com>
2023-04-01 11:10:45 -0300

    arreglo nav y footer

--- src/main/java/com/foodieparty/fodieParty/RestauranteFoodiePartyApplication.java
+++ src/main/java/com/foodieparty/fodieParty/RestauranteFoodiePartyApplication.java
@@ -45,20 +45,22 @@ public class RestauranteFoodiePartyApplication {
 			usuarioRepositorio.save(juan);
 			Usuario daniel = new Usuario("daniel", "lopez", "dlopez1591@gmail.com", passwordEncoder.encode("daniel123"), "123456");
 			usuarioRepositorio.save(daniel);
-			Bebida bebida1 = new Bebida("Jugo","Jugo de naranja natural","https://firebasestorage.googleapis.com/v0/b/delicatezza-20007.appspot.com/o/42bb3965-1df3-47cf-a0d4-cb436a9601a2.jpg?alt=media", TipoBebida.AGUA,40,100.0);
-			Bebida bebida2 = new Bebida("Birra","vaso de birra ipa","https://firebasestorage.googleapis.com/v0/b/delicatezza-20007.appspot.com/o/9b789c94-465b-48c5-b9d3-7fb22d24c3f0.jpg?alt=media", TipoBebida.ALCOHOLICA,40,350.0);
+			Bebida bebida1 = new Bebida("Jugo","Jugo de naranja natural",
+					"https://firebasestorage.googleapis.com/v0/b/delicatezza-20007.appspot.com/o/42bb3965-1df3-47cf-a0d4-cb436a9601a2.jpg?alt=media", TipoBebida.AGUA,40,100.0);
+			Bebida bebida2 = new Bebida("Birra","vaso de birra ipa",
+					"https://firebasestorage.googleapis.com/v0/b/delicatezza-20007.appspot.com/o/9b789c94-465b-48c5-b9d3-7fb22d24c3f0.jpg?alt=media", TipoBebida.ALCOHOLICA,40,350.0);
 			bebidaRepositorio.save(bebida1);
 			bebidaRepositorio.save(bebida2);
-			Comida comida1 = new Comida("Panchito","Panchito con adereso y papitas", TipoComida.PRINCIPAL,250.0,"https://firebasestorage.googleapis.com/v0/b/delicatezza-20007.appspot.com/o/dead24cf-2065-4e92-9ba4-27ed2b9c42e7.jpg?alt=media",true);
-			Comida comida2 = new Comida("Burguer","Hamburguesa Doble Queso", TipoComida.PRINCIPAL,450.0,"https://firebasestorage.googleapis.com/v0/b/delicatezza-20007.appspot.com/o/0e97cb9c-4252-4875-9143-658e39d91ff3.jpg?alt=media",true);
+			Comida comida1 = new Comida("Panchito","Panchito con adereso y papitas", TipoComida.PRINCIPAL,250.0,
+					"https://firebasestorage.googleapis.com/v0/b/delicatezza-20007.appspot.com/o/dead24cf-2065-4e92-9ba4-27ed2b9c42e7.jpg?alt=media",true);
+			Comida comida2 = new Comida("Burguer","Hamburguesa Doble Queso", TipoComida.PRINCIPAL,450.0,
+					"https://firebasestorage.googleapis.com/v0/b/delicatezza-20007.appspot.com/o/0e97cb9c-4252-4875-9143-658e39d91ff3.jpg?alt=media",true);
 			comidaRepositorio.save(comida1);
 			comidaRepositorio.save(comida2);
-
 			Comida postre1 = new Comida ("Helado","bochita",TipoComida.POSTRE,500.0,
 					"https://firebasestorage.googleapis.com/v0/b/delicatezza-20007.appspot.com/o/6f171418-8f6d-46e5-b15a-d677df87f102.jpg?alt=media",true);
 			Comida postre2 = new Comida ("Flan","un flansito",TipoComida.POSTRE,800.0,
 					"https://firebasestorage.googleapis.com/v0/b/delicatezza-20007.appspot.com/o/caa98a39-ea5a-4173-b514-c3c1b66fb500.jpg?alt=media",true);
-
 			comidaRepositorio.save(postre1);
 			comidaRepositorio.save(postre2);
 
